ec73190b6434c12ee2d945aa103de55bc024c64b
Alexandre Zagame <zagamealexandre@gmail.com>
2021-03-11 14:13:16 +0100

    Start Peers and Broadcast Chain on API Mine

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const Block = require('./block');
+const PubSub = require('./pubsub');
 
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 app.use(bodyParser.json());
 
@@ -17,10 +21,19 @@ app.post('/api/mine', (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect('/api/blocks');
 });
 
-const PORT = 3000;
-app.listen(3000, () => {
+const DEFAULT_PORT = 3000;
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+app.listen(PORT, () => {
   console.log(`listening at localhost:${PORT}`);
 });
--- pubsub.js
+++ pubsub.js
@@ -1,15 +1,57 @@
+// const PubNub = require('pubnub');
+
+// const credentials = {
+//   publishKey: 'pub-c-2596ca7e-f13f-43be-8074-088f1bbba279',
+//   subscribeKey: 'sub-c-425e8d9e-8264-11eb-99bb-ce4b510ebf19',
+//   secretKey: 'sec-c-YzZjMWVmMzMtNGJmMy00MTJjLThkZGUtYjVhOGM0N2YwMTI5',
+// };
+
+// const CHANNELS = {
+//   TEST: 'TEST',
+// };
+
+// class PubSub {
+//   constructor() {
+//     this.pubnub = new PubNub(credentials);
+
+//     this.pubnub.subscribe({ channels: [Object.values(CHANNELS)] });
+
+//     this.pubnub.addListener(this.listener());
+//   }
+
+//   listener() {
+//     return {
+//       message: (messageObject) => {
+//         const { channel, message } = messageObject;
+//         console.log(
+//           `Message received. Channel: ${channel}. Message: ${message}`
+//         );
+//       },
+//     };
+//   }
+
+//   publish({ channel, message }) {
+//     this.pubnub.publish({ channel, message });
+//   }
+// }
+
+// module.exports = PubSub;
+
 const redis = require('redis');
 
 const CHANNELS = {
   TEST: 'TEST',
+  BLOCKCHAIN: 'BLOCKCHAIN',
 };
 
 class PubSub {
-  constructor() {
+  constructor({ blockchain }) {
+    this.blockchain = blockchain;
+
     this.publisher = redis.createClient();
     this.subscriber = redis.createClient();
 
-    this.subscriber.subscribe(CHANNELS.TEST);
+    this.subscribeToChannels();
 
     this.subscriber.on('message', (channel, message) => {
       this.handleMessage(channel, message);
@@ -18,9 +60,30 @@ class PubSub {
 
   handleMessage(channel, message) {
     console.log(`Message received. Channel: ${channel}. Message: ${message}`);
+
+    const parsedMessage = JSON.parse(message);
+
+    if (channel === CHANNELS.BLOCKCHAIN) {
+      this.blockchain.replaceChain(parsedMessage);
+    }
+  }
+
+  subscribeToChannels() {
+    Object.values(CHANNELS).forEach((channel) => {
+      this.subscriber.subscribe(channel);
+    });
+  }
+
+  publish({ channel, message }) {
+    this.publisher.publish(channel, message);
+  }
+
+  broadcastChain() {
+    this.publish({
+      channel: CHANNELS.BLOCKCHAIN,
+      message: JSON.stringify(this.blockchain.chain),
+    });
   }
 }
 
-const testPubSub = new PubSub();
-
-setTimeout(() => testPubSub.publisher.publish(CHANNELS.TEST, 'foo'), 1000);
+module.exports = PubSub;
