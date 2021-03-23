e9b67c927dadac35e0daae2822de86f9d0ae5f41
Alexandre Zagame <zagamealexandre@gmail.com>
2021-03-23 10:54:00 +0100

    wirintting the tests

--- app/transaction-miner.js
+++ app/transaction-miner.js
@@ -1,6 +1,16 @@
 class TransactionMiner {
+  constructor({ blockchain, transactionPool, wallet, pubsub }) {
+    this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
+    this.wallet = wallet;
+    this.pubsub = pubsub;
+  }
   mineTransactions() {
     // get the transaction pools valid transactions
+    // generate the miner's reward
+    // add a block consisisting of these transactions to the blockchain
+    // broadcast the updated blockchain
+    // clear the transaction pool
   }
 }
 
--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,6 +1,7 @@
 const TransactionPool = require('./transaction-pool');
 const Transaction = require('./transaction');
 const Wallet = require('./index');
+const TransactionMiner = require('../app/transaction-miner');
 
 describe('TransactionPool', () => {
   let transactionPool, transaction, senderWallet;
@@ -30,6 +31,38 @@ describe('TransactionPool', () => {
           inputAddress: senderWallet.publicKey,
         })
       ).toBe(transaction);
+    });
+  });
+
+  describe('validTransactions()', () => {
+    let validTransactions, errorMock;
+
+    beforeEach(() => {
+      validTransactions = [];
+      errorMock = jest.fn();
+      global.console.error = errorMock;
+
+      for (let i = 0; i < 10; i++) {
+        transaction = new Transaction({
+          senderWallet,
+          recipient: 'any-recipient',
+          amount: 30,
+        });
+
+        if (i % 3 === 0) {
+          transaction.input.amount = 999999;
+        } else if (i % 3 === 1) {
+          transaction.input.signature = new Wallet().sign('foo');
+        } else {
+          validTransactions.push(transaction);
+        }
+
+        transactionPool.setTransaction(transaction);
+      }
+    });
+
+    it('returns a valid transaction', () => {
+      expect(transactionPool.validTransactions()).toEqual(validTransactions);
     });
   });
 });
